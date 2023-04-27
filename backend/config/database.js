959c5cd7a95d3361e5df1384f3c0da49855cf22a
Muhammad Basyuny <mohamedfahmyrf20@gmail.com>
2023-04-27 23:27:50 +0200

    Register Backend

---
+++ backend/config/database.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const databaseConnect = () => {
+    mongoose.connect(process.env.DATABASE_URL,{
+        useNewURLParser: true,
+        useUnifiedTopology: true
+    }).then (() => {console.log('Mongodb Database Connected')})
+    .catch (error => console.log(error))
+}
+
+module.exports = databaseConnect;
--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,17 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+const databaseConnect = require('./config/database');
+const dotenv = require('dotenv');
+
+dotenv.config({
+    path:'backend/config/config.env'
+})
 
 app.get('/', (req,res) => {
     res.send(`<h1>This is from Backend Server</h1>`);
 })
+databaseConnect();
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
